7c4a2f6c613e3eeaf0646b2aaa8a2d2147e8759e
dariapydorenko <daria.pydorenko@Sophos.com>
2018-06-08 12:38:12 +0300

    add jacoco

--- src/test/kotlin/com/example/venka/demo/DemoApplicationTest.kt
+++ src/test/kotlin/com/example/venka/demo/DemoApplicationTest.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTest {
 
     @Test
     fun contextLoads() {
