b2ed2da5433a6482fd25350499e68aaa2481b503
Daria Pydorenko <venka.daria@gmail.com>
2018-07-13 13:05:23 +0300

    add flairs

--- src/main/kotlin/com/example/venka/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/venka/demo/DemoApplication.kt
@@ -14,5 +14,5 @@ fun main(args: Array<String>) {
 // Controller advice ?
 // frontend work when load post
 // dup code remove
-// gradle commands
+// gradle commands + task for each pair glue-package and union this tasks
 // deps level(compile...)
